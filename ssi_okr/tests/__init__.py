09b9677053633ede7de78804e3a8a9c84cc5fbb7
Andhitia Rama <andhitia.r@gmail.com>
2026-08-16 20:10:46 +0700

    [UPD] ssi_okr
    * Tambah enam tour UI test model okr_objective (create, ready, start,
      confirm, approve, cancel) di static/tests/tours/okr_objective_tour.js
    * Tambah tests/test_ui_okr_objective.py sebagai HttpSavepointCase yang
      menyiapkan Pre-Condition tiap Instruksi Kerja dan menjalankan tour-nya
    * Daftarkan aset tour ke bundle web.assets_tests lewat views/assets.xml
    * Tambah dependensi web_tour di manifest

--- ssi_okr/tests/__init__.py
+++ ssi_okr/tests/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2026 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import test_okr_objective
+from . import test_okr_objective, test_ui_okr_objective
