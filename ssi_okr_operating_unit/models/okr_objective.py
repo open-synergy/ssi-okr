5a7565ff9f2267064fc3a604bee47ebc59ce0e15
Andhitia Rama <andhitia.r@gmail.com>
2026-08-16 19:14:17 +0700

    [UPD] ssi_okr_operating_unit
    * Pindahkan direktori view/ menjadi views/ dan selaraskan path pada
      key data di __manifest__.py
    * Tambahkan docstring pada class OkrKeyResult, OkrObjective, dan
      TestOkrOperatingUnit beserta method test_okr_operating_unit

--- ssi_okr_operating_unit/models/okr_objective.py
+++ ssi_okr_operating_unit/models/okr_objective.py
@@ -6,6 +6,12 @@ from odoo import models
 
 
 class OkrObjective(models.Model):
+    """
+    Attaches ``mixin.single_operating_unit`` to the OKR objective model.
+    Adds the Operating Unit field to objectives and enforces the record
+    rules that scope objective access by Operating Unit.
+    """
+
     _name = "okr_objective"
     _inherit = [
         "okr_objective",
